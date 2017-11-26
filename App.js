5f4f87a7d2201ea46074b6f5aad9c6529d5812c0
FastNinja <alex.maslyukov@myob.com>
2017-11-26 23:34:37 +1100

    playing with react native and layout

---
+++ App.js
@@ -0,0 +1,176 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Button, Image, TouchableHighlight, Alert } from 'react-native';
+import { AppLoading, Font } from 'expo';
+import { style } from 'expo/src/Font';
+
+
+const myButtonStyles = StyleSheet.create({
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#2196F3'
+  },
+
+  buttonText: {
+    padding: 8,
+    color: 'white'
+  },
+
+  linkButton: {
+    alignItems: 'center',
+  },
+
+  linkButtonText: {
+    padding: 8,
+  }
+
+});
+
+const MyHr = ({ color, style = {} }) => (
+  <View
+    style={[{
+      borderBottomColor: '#C9C9C9',
+      borderBottomWidth: 1,//StyleSheet.hairlineWidth
+
+    }, style]}
+  />
+)
+
+const MyButton = ({ title, onPress, style = {} }) => (
+  <TouchableHighlight onPress={onPress} underlayColor="white">
+    <View style={[myButtonStyles.button, styles.roundedBorder, style]}>
+      <Text style={[myButtonStyles.buttonText, styles.base]}>{title}</Text>
+    </View>
+  </TouchableHighlight>
+)
+
+const MyLinkButton = ({ title, onPress, style = {} }) => (
+  <TouchableHighlight onPress={onPress} underlayColor="white">
+    <View style={[myButtonStyles.linkButton, style]}>
+      <Text style={[myButtonStyles.linkButtonText, styles.base]}>{title}</Text>
+    </View>
+  </TouchableHighlight>
+)
+
+
+export default class App extends React.Component {
+  state = {
+    loaded: false,
+  };
+
+  componentWillMount() {
+    this._loadAssetsAsync();
+  }
+
+  _loadAssetsAsync = async () => {
+    await Font.loadAsync({
+      montserrat: require('./assets/fonts/Montserrat-Regular.ttf'),
+    });
+    this.setState({ loaded: true });
+  };
+
+  onLogin() {
+    Alert.alert('Login!');
+  }
+
+  onForget() {
+    Alert.alert('forget!');
+  }
+
+  onSignup() {
+    Alert.alert('signup!');
+  }
+
+  render() {
+    if (!this.state.loaded) {
+      return <View style={styles.container} />
+    }
+    return (
+      <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+
+        <View style={[styles.container]}>
+
+          <View style={styles.centerChild} >
+            <Image
+              style={[styles.logo, styles.mb4, { marginTop: 100 }]}
+              source={require('./images/logo.png')}
+            />
+          </View>
+          <View style={[styles.roundedBorder, styles.mb4]}>
+            <TextInput style={styles.base} keyboardType="email-address" placeholder="Email Address"></TextInput>
+          </View>
+          <View style={[styles.roundedBorder, styles.mb4]}>
+            <TextInput secureTextEntry={true} returnKeyType="go" style={[styles.base]} placeholder="Password"></TextInput>
+          </View>
+
+
+          <MyButton style={styles.mb4} title="Login" onPress={this.onLogin}> </MyButton>
+
+          <MyLinkButton style={styles.mb4} title="Forget Password?" onPress={this.onForget}> </MyLinkButton>
+
+          <MyHr style={styles.mb4} />
+
+          <View style={styles.centerChild} >
+            <Text style={[styles.base, styles.smallText]} >
+              Dont have an account?
+               </Text>
+          </View>
+
+          <MyLinkButton style={styles.mb4} title="Sign Up!" onPress={this.onSignup}> </MyLinkButton>
+
+        </View>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+  centerChild: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    maxWidth: 300,
+  },
+
+  logo: {
+    width: 150,
+    height: 103
+  },
+
+  mb4: {
+    marginBottom: 24,
+  },
+
+  mt4: {
+    marginTop: 24,
+  },
+
+  roundedBorder: {
+    borderColor: '#D9D9D9',
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderRadius: 10,
+    paddingLeft: 12,
+    paddingRight: 12,
+    paddingTop: 8,
+    paddingBottom: 8
+  },
+
+  base: {
+    // borderColor: 'red',
+    // borderWidth: 1,
+    // borderStyle: 'solid',
+    fontFamily: 'montserrat',
+    fontSize: 20,
+  },
+  smallText: {
+
+    fontSize: 14,
+  },
+});
